46caa8d3deb7c9ae8a5da9050f9bc2971aabbe3f
saeid-dousti86 <61210011+saeid-dousti86@users.noreply.github.com>
2020-03-19 09:45:39 -0400

    Update input_path.py

--- src/input_path.py
+++ src/input_path.py
@@ -5,5 +5,5 @@ def input_path_func():
     This function retrieves the path of the input file from the repository
     :return: the input file absolute path
     '''
-    input_folder_path = os.path.abspath('../input')
+    input_folder_path = os.path.abspath('./input')
     return os.path.join(input_folder_path,'complaints.csv')
