a866fb360b7d0374d4397a420e2e2092be7429fa
saeid-dousti86 <61210011+saeid-dousti86@users.noreply.github.com>
2020-03-19 10:07:01 -0400

    adding comments

--- src/cmp_main_list.py
+++ src/cmp_main_list.py
@@ -1,6 +1,9 @@
 import re
 '''
-In this functions file, the complaints.csv file read into a list.
+- In this functions file, the complaints.csv file read into a list of dictionaries, one dictionary for each line of complaint
+- In the line parsing, the proper end of line is distinguished from the others by matching date format of XXXX-XX-XX of the next line
+- To parse the complaint lines properly, the commas between quotation marks are replaced with @#$#@
+- Finally after creating the main_list, commas are replacing @#$#@
 '''
 def cmp_main_list_func(in_path):
 
@@ -23,7 +26,8 @@ def cmp_main_list_func(in_path):
             dict_row = dict()
             for kk, Header in enumerate(keys):
                 dict_row[Header] = list_row[kk]
-
+            
+            # a new key named "Product/Year" is defined to facilitate the final sorting based on product and year all at once
             dict_row['Product/Year'] = (dict_row['Product'].strip() + ',' + re.findall(r'\d{4}', dict_row['Date received'])[0]).replace('@#$#@',',').lower()  # make the product/Year all lower case
 
             main_list.append(dict_row)
@@ -34,6 +38,9 @@ def cmp_main_list_func(in_path):
     return main_list, Error_list
 
 def split_w_comma_func(line_string,length):
+    '''
+    - spliting each complaint line by commas
+    '''
     list_line = line_string.strip().split(',')
     return list_line
 
